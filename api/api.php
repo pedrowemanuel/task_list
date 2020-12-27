289af730353b6c9f010324009e2b4f2250b7a8e6
pedrowemanuel <pedronuel36@gmail.com>
2020-12-27 12:06:21 -0300

    config headers - backend

--- api/api.php
+++ api/api.php
@@ -1,4 +1,9 @@
 <?php
+
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+    
     include 'src/database/Connection.php';
     include 'src/router.php';
 
--- api/src/router.php
+++ api/src/router.php
@@ -1,8 +1,4 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
-
     include_once 'controllers/ControllerTasks.php';
     include_once 'models/Tasks.php';
     $controllerTask = new ControllerTasks();
